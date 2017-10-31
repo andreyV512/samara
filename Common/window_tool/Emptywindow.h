7ff5465b156a507a2231ecbc01331cccbbbdcce4
Andrew90 <drewan@rambler.ru>
2017-10-31 12:30:01 +0500

    Merge branch 'messages' of //Docstation/soft/scrapyard/repository/USPCSamara_Buran_9955_143_10_1626 into messages
    # Conflicts:
    #	Common/Base/ExpressBase.hpp

--- Common/window_tool/Emptywindow.h
+++ Common/window_tool/Emptywindow.h
@@ -4,7 +4,7 @@
 #include "window_tool/message.h"
 #include "resource.h"
 #include "templates/WindowsEventTemplate.hpp"
-#include "DiffApp\App.h"
+#include "AppMess\App.h"
 //---------------------------------------------------------------------------------
 ATOM MyRegisterClass(WNDPROC WndProc
 	, const wchar_t *windowClassName
